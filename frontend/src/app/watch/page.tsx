6592c5c05470c3755f92662d55b5d71551cde5e5
monjurul-190629 <monjurulalam190629@gmail.com>
2026-03-14 02:41:47 +0600

    send: video_id to backend

--- frontend/src/app/watch/page.tsx
+++ frontend/src/app/watch/page.tsx
@@ -2,6 +2,9 @@
 
 import useYouTubePlayer from "@/hooks/useYouTubePlayer";
 import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect } from "react";
+
+const FASTAPI_ENDPOINT = "http://localhost:8002/api/video-events/";
 
 const page = () => {
   const searchParams = useSearchParams();
@@ -19,6 +22,43 @@ const page = () => {
     startTime,
     interval,
   });
+
+  // useCallBack -> fetch -> FastApi -> timeScleDB
+  // useEffect
+  // useDebouncher
+
+  const updateBackend = useCallback(
+    async (currentPlayerState: any) => {
+      const headers = {
+        "Content-Type": "application/json",
+      };
+      // console.log(video_id, currentPlayerState)
+
+      try {
+        const response = await fetch(FASTAPI_ENDPOINT, {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify({ ...currentPlayerState, video_id: video_id }),
+        });
+        if (!response.ok) {
+          console.log(await response.text());
+          console.log("error adding data to the backend");
+        } else {
+          const responseData = await response.json();
+          console.log("db data is", responseData);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [video_id],
+  );
+
+  useEffect(() => {
+    if (!playerState.isReady) return;
+    if (playerState.video_state_label === "CUED") return;
+    updateBackend(playerState);
+  }, [playerState]);
 
   return (
     <div className="w-[50vw] mx-auto h-full px-5">
