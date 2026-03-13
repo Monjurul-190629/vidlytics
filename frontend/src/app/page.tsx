afe75f27e49ee9b9c0f0ae02bd8e77c1cfefd808
monjurul-190629 <monjurulalam190629@gmail.com>
2026-03-14 01:52:47 +0600

    change: design of Home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,60 +1,21 @@
-import Image from "next/image";
+import YoutubeUrlForm from "@/components/YoutubeUrlForm";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="NextJs Logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/7fIvHuqiLws?si=c6tsDXX60JFCBkrd"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
+      <div className="flex flex-col items-center justify-center flex-1 w-full max-w-2xl px-4">
+        <div className="mb-8 inline-flex space-x-2 items-center">
+          <h1 className="text-6xl font-bold text-center bg-red-600 rounded-md p-3">
+            <span className="text-white">YouTube</span>
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to ok
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+          <h1 className="text-6xl font-bold text-center">
+            <span className="text-gray-800"> Player</span>
+          </h1>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        <main className="w-full">
+          <YoutubeUrlForm />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/YoutubeUrlForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const YoutubeUrlForm = () => {
+  return (
+    <div>YoutubeUrlForm</div>
+  )
+}
+
+export default YoutubeUrlForm
