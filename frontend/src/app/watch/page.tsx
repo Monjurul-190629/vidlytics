c21011e0eccfbae8d39fbf0ab3a4a9a9582149fb
monjurul-190629 <monjurulalam190629@gmail.com>
2026-03-13 23:16:25 +0600

    complete: video set up

--- frontend/src/app/watch/page.tsx
+++ frontend/src/app/watch/page.tsx
@@ -7,13 +7,18 @@ const page = () => {
   const searchParams = useSearchParams();
   const video_id = searchParams.get("v") ?? "";
 
-  useYouTubePlayer({ video_id, elementId: video_id });
+  const playerElementId = "youtube-player";
+
+  useYouTubePlayer({ video_id, elementId: playerElementId });
 
   return (
     <div className="w-[50vw] mx-auto h-full px-5">
       <div id="video-container" className="relative w-full">
         <div className="relative w-full pt-[56.25%] bg-black">
-          <div id={video_id} className="absolute top-0 left-0 w-full h-full" />
+          <div
+            id={playerElementId}
+            className="absolute top-0 left-0 w-full h-full"
+          />
         </div>
       </div>
       <h1>Watch : {video_id}</h1>
