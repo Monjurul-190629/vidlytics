0f9ee793e6773603f557ab605fb71663db1c0cbc
monjurul-190629 <monjurulalam190629@gmail.com>
2026-03-14 16:25:48 +0600

    add: YoutubePlayerState Model and WatchEventResponse Model

---
+++ backend/src/api/video_events/models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import Optional
+
+from timescaledb import TimescaleModel
+from pydantic import BaseModel, Field as PydanticField
+from sqlmodel import SQLModel, Field
+
+
+class YouTubePlayerState(SQLModel, table=False):
+    is_ready: bool 
+    video_id: str = Field(index=True)
+    video_title: str
+    current_time: float
+    video_state_label: str
+    video_state_value: int
+
+
+
+class YouTubeWatchEventResponseModel(SQLModel, table=False):
+    id: int = Field(primary_key=True)
+    video_id: str = Field(index=True)
+    current_time: float
+    time: datetime
