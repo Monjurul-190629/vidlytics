b4076000c5a3a37e4a94b0cc216404740e2534f2
monjurul-190629 <monjurulalam190629@gmail.com>
2026-03-14 16:17:08 +0600

    add: origins

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,26 @@
+import os
 from contextlib import asynccontextmanager
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+host_origin = ""
+host_origin_portless = ""
+
+
+HOST = os.environ.get("HOST")
+HOST_SCHEME = os.environ.get("HOST_SCHEME")
+HOST_PORT = os.environ.get("HOST_PORT")
+
+if all([HOST, HOST_SCHEME, HOST_PORT]):
+    host_origin = f"{HOST_SCHEME}://{HOST}:{HOST_PORT}"
+    host_origin_portless = f"{HOST_SCHEME}://{HOST}"
+
+origins = [
+    host_origin,
+    host_origin_portless
+]
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,11 +30,18 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World my old friend I did it"}
+    return {"Hello": "Done it"}
 
 
 @app.get("/healthz")
