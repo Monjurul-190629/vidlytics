e85398ce2a805f3087414594aa2823ee888f263c
monjurul-190629 <monjurulalam190629@gmail.com>
2026-03-31 23:16:58 +0600

    feat: youtube watch event modle

--- backend/src/api/video_events/models.py
+++ backend/src/api/video_events/models.py
@@ -6,6 +6,17 @@ from pydantic import BaseModel, Field as PydanticField
 from sqlmodel import SQLModel, Field
 
 
+class YouTubeWatchEvent(TimescaleModel, table=True):
+    # id: int = Field(primary_key=True)
+    is_ready: bool 
+    video_id: str = Field(index=True)
+    video_title: str
+    current_time: float
+    video_state_label: str
+    video_state_value: int
+    referer: Optional[str] = Field(default="", index=True)
+    watch_session_id: Optional[str] = Field(index=True)
+
 class YouTubePlayerState(SQLModel, table=False):
     is_ready: bool 
     video_id: str = Field(index=True)
